646c3fddf4e0fcd19d8c9eb15b57c8b75738c584
Laureano Sanchez <laureano.gabriel.aguiar@ibm.com>
2021-01-07 11:48:07 -0600

    Copy image script updated

--- copy_image.sh
+++ copy_image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cp ~/Downloads/Clientes/Estilo\ de\ vida/RaquelRovira-050.jpg ~/Projects/github.com/vue-photographer-page/src/assets/images/places
+cp ~/Downloads/Portafolio/bxl* ~/Projects/github.com/vue-photographer-page/src/assets/icons/
