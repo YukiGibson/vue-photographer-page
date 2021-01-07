5c146c99468c4821283502c035ccf6a3d91619f4
Laureano Sanchez <laureano.gabriel.aguiar@ibm.com>
2021-01-07 11:47:12 -0600

    Update gitignore and about

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,21 +1,33 @@
 <template>
-  <p>
-    Raquel Rovira Díaz es una joven nacida en San José, Costa Rica en agosto de 1991. Atraída desde muy niña por las
-    artes visuales y sin tener muy claro el rumbo que deseaba tomar, empieza su carrera a los 20 años al estudiar Post
-    Producción Audiovisual y Publicidad, carreras que vio como instrumentos para contar todas esas historias que tanto
-    volaban por su cabeza. Siempre se vio atraída por lo diferente, lo "raro", y todo eso que podía crear con sus
-    propias manos. Detallista y minuciosa, un tanto irreverente según algunos de profesores, pero siempre con una
-    jovialidad notable, Raquel busca exponer la belleza de aquello que otros podrían tomar como imperfecto.
-    Paralelamente a sus estudios universitarios, Raquel disfrutaba fotografiar conciertos locales en donde aprendería
-    sola, a prueba y error de fotografía, pero no es sino, hasta el 2019, que decide dejar el mundo publicitario y
-    comenzar formalmente sus estudios fotográficos para dedicarse, ahora sí, por completo a la fotografía.
-  </p>
+  <div>
+    <div>
+      <img class="rounded mx-auto d-block image-shadow" :src="require(`@/assets/images/Raquel_Rovira_Costa_Rica.jpg`)" alt="raquel rovira foto" />
+    </div>
+    <br />
+    <p class="text-dark text-md-center p-md-3">
+      <b>Raquel Rovira Díaz</b> es una joven nacida en San José, Costa Rica en agosto de 1991. Atraída desde muy niña por las
+      artes visuales y sin tener muy claro el rumbo que deseaba tomar, empieza su carrera a los 20 años al estudiar Post
+      Producción Audiovisual y Publicidad, carreras que vio como instrumentos para contar todas esas historias que tanto
+      volaban por su cabeza. Siempre se vio atraída por lo diferente, lo "raro", y todo eso que podía crear con sus
+      propias manos. Detallista y minuciosa, un tanto irreverente según algunos de profesores, pero siempre con una
+      jovialidad notable, Raquel busca exponer la belleza de aquello que otros podrían tomar como imperfecto.
+      Paralelamente a sus estudios universitarios, Raquel disfrutaba fotografiar conciertos locales en donde aprendería
+      sola, a prueba y error de fotografía, pero no es sino, hasta el 2019, que decide dejar el mundo publicitario y
+      comenzar formalmente sus estudios fotográficos para dedicarse, ahora sí, por completo a la fotografía.
+    </p>
+  </div>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
+import Vue from 'vue';
 
-  export default Vue.extend({
-    name: 'About',
-  });
+export default Vue.extend({
+  name: 'About',
+});
 </script>
+
+<style scoped>
+img {
+  width: 50%;
+}
+</style>
