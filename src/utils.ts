5242e4a2d9e793e7a77aa0fc948f3c93327f6289
Laureano Sanchez <laureano.gabriel.aguiar@ibm.com>
2021-01-08 18:21:42 -0600

    Added location for favicon icon in nginx.conf

--- src/utils.ts
+++ src/utils.ts
@@ -40,7 +40,6 @@ export function getTarotistImages(): string[] {
 
 export function getArianaJoyeriaImages(): string[] {
   const context = require.context('./assets/images/clients/ariana_joyeria', false, /(?:jpg|gif|png)/);
-  console.log(context.keys());
   return getImages(context);
 }
 
