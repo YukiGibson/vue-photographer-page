a8b4bb926b1462e03e38b856b7a5c3227b8a9092
Laureano Sanchez <laureano.gabriel.aguiar@ibm.com>
2021-02-16 16:15:15 -0600

    Added metatags

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,34 @@
 import Vue from 'vue';
 import {BootstrapVue, IconsPlugin} from 'bootstrap-vue';
+import VueMeta from 'vue-meta';
 import App from './App.vue';
 import router from './router';
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
+Vue.use(VueMeta);
 
 const vm = new Vue({
   el: `#app`,
   data: function() {
     return {
       title: 'Raquel Rovira Photography',
+    };
+  },
+  metaInfo() {
+    return {
+      title: 'Raquel Rovira Photography',
+      meta: [
+        {
+          name: 'description',
+          content:
+            'Raquel Rovira Photography',
+        },
+        {property: 'og:title', content: 'Raquel Rovira Photography'},
+        {property: 'og:site_name', content: 'Raquel Rovira Photography'},
+        {property: 'og:type', content: 'website'},
+        {name: 'robots', content: 'index,follow'},
+      ],
     };
   },
   components: {
